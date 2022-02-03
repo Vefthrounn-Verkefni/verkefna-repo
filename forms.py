01a87d6bddb65a80c671a2df690410edb31725a6
thordur03 <thorduringis@gmail.com>
2022-02-03 23:45:50 +0000

    Create_user

--- forms.py
+++ forms.py
@@ -1,10 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField,SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms import Form, BooleanField, StringField, PasswordField
+from wtforms.validators import DataRequired,Length
+
 
 class CreateUser(FlaskForm):
-    username = TextAreaField("Username", validators = [Length(min=4, max=25)])
-    email = TextAreaField("Email Address", validators = [Length(min=6, max=35)])
-    password = TextAreaField("New Password", validators = [Length(min=6, max=35)])
-    submit = SubmitField("Create User")
+    username = StringField('Username', validators = [DataRequired()])
+    email = StringField("Email",validators = [DataRequired()])
+    password = PasswordField('Password', validators = [DataRequired()])
+    
+
     
