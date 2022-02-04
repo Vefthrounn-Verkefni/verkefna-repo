b48cb6932f96363a160d6ff55d7946488b3a01a9
thordur03 <thorduringis@gmail.com>
2022-02-04 09:55:21 +0000

    database

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
 from flask import Flask, render_template, request,redirect, url_for
-#from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from forms import CreateUser
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "password"
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/test.db"
-#db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/database.db"
+db = SQLAlchemy(app)
+
+class UserModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(240))
+    email = db.Column(db.String(385))
+    password = db.Column(db.String(240))
+
+
+    def __str__(self):
+        return f"{self.id},{self.name} {self.email}"
 
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return render_template("index.html")
+    users = UserModel.query.all()
+    return render_template("index.html", users=users)
 
 @app.route("/create_user",methods=["GET","POST"])
 def create_user():
     create_user = CreateUser()
     if create_user.validate_on_submit():
-        print(create_user.username)
+        newUser = UserModel(name=create_user.name.data,email=create_user.email.data,password=create_user.password.data)
+        db.session.add(newUser)
+        db.session.commit()
     return render_template("CreateUser.html",form=create_user)
 
 if __name__ == "__main__":
