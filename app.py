55328c94e8a9d0ae02f47623fffb2846545bc057
thordur03 <thorduringis@gmail.com>
2022-02-17 17:39:20 +0000

    profile pic

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from forms import CreateUser, LoginForm,EditUser
 from werkzeug.utils import secure_filename
 import uuid as uuid
 import os
+
 # Flask Uppseting
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "password"
@@ -37,19 +38,16 @@ class UserModel(db.Model,UserMixin):
     profile_picture = db.Column(db.String(400),default="n/a")
     date_added = db.Column(db.DateTime,default=datetime.utcnow())
     password_hash = db.Column(db.String(128), nullable=False)
-    @property
-    def password(self):
-        raise AttributeError("Password is not a readable attribute")
-    @password.setter
-    def password(self,password):
-        self.password_hash = generate_password_hash(password)
-
-    def verify_password(self,password):
-        return check_password_hash(self.password_hash,password)
 
     def __repr__(self):
         return "<Name %r>" % self.name
-
+""" 
+class ClothingModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    describtion = db.Column(db.String(220))
+    type = db.Column(db.String(10), nullable=False)
+    image_link = db.Column(db.String(),nullable=False)
+"""
 @app.route("/",methods=["GET","POST"])
 def index():
     users = UserModel.query.all()
@@ -106,7 +104,11 @@ def create_user():
             name=signUpForm.name.data = ""
             username=signUpForm.username.data = ""
             email=signUpForm.email.data = ""
+
             flash("User added","success")
+            login_user(user)
+
+            return redirect("dashboard")
 
     return render_template("CreateUser.html",form=signUpForm)
 
