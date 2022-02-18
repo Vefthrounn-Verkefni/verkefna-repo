a9ff07682ecde20924b3119d8f86863c737e5487
thordur03 <thorduringis@gmail.com>
2022-02-18 01:11:45 +0000

    Branch til að læra á myndvinslu

--- aefing.py
+++ aefing.py
@@ -1,36 +1,22 @@
 import cv2
 import numpy as np
-img = cv2.imread('static/img/testimage.jpg')
-hh, ww = img.shape[:2]
+from matplotlib import pyplot as plt
 
-# threshold on white
-# Define lower and uppper limits
-lower = np.array([200, 200, 200])
-upper = np.array([255, 255, 255])
+image_bgr = cv2.imread('static\img\testimage.jpg')
+image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
 
-# Create mask to only select black
-thresh = cv2.inRange(img, lower, upper)
+rectangle = (0, 0, 300, 380)
+# 600, 550, 1150, 2000
+mask = np.zeros(image_rgb.shape[:2], np.uint8)
 
-# apply morphology
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20,20))
-morph = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+bgdModel = np.zeros((1, 65), np.float64)
+fgdModel = np.zeros((1, 65), np.float64)
 
-# invert morp image
-mask = 255 - morph
+cv2.grabCut(image_rgb, mask, rectangle, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
 
-# apply mask to image
-result = cv2.bitwise_and(img, img, mask=mask)
+mask_2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
 
+image_rgd_nobg = image_rgb * mask_2[:, :, np.newaxis]
 
-# save results
-cv2.imwrite('pills_thresh.jpg', thresh)
-cv2.imwrite('pills_morph.jpg', morph)
-cv2.imwrite('pills_mask.jpg', mask)
-cv2.imwrite('pills_result.jpg', result)
-
-cv2.imshow('thresh', thresh)
-cv2.imshow('morph', morph)
-cv2.imshow('mask', mask)
-cv2.imshow('result', result)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+plt.imshow(image_rgd_nobg), plt.axis('off')
+plt.show()
--- app.py
+++ app.py
@@ -91,40 +91,7 @@ def uploadClothing():
     imageForm = uploadImage()
     if uploadImage.validate_on_submit:
         image = imageForm.image.data
-        img = cv2.imread('static/img/testimage.jpg')
-        hh, ww = img.shape[:2]
 
-        # threshold on white
-        # Define lower and uppper limits
-        lower = np.array([200, 200, 200])
-        upper = np.array([255, 255, 255])
-
-        # Create mask to only select black
-        thresh = cv2.inRange(img, lower, upper)
-
-        # apply morphology
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20,20))
-        morph = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-
-        # invert morp image
-        mask = 255 - morph
-
-        # apply mask to image
-        result = cv2.bitwise_and(img, img, mask=mask)
-
-
-        # save results
-        cv2.imwrite('pills_thresh.jpg', thresh)
-        cv2.imwrite('pills_morph.jpg', morph)
-        cv2.imwrite('pills_mask.jpg', mask)
-        cv2.imwrite('pills_result.jpg', result)
-
-        cv2.imshow('thresh', thresh)
-        cv2.imshow('morph', morph)
-        cv2.imshow('mask', mask)
-        cv2.imshow('result', result)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
 
         return render_template("uploadClothing.html",form=imageForm)
     return render_template("uploadClothing.html",form=imageForm)
